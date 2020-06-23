7f180d43292b258ecf5a6fa9b2a94db4ca6c5597
Biguit <leonardo.biguinatti@etec.sp.gov.br>
2020-06-22 23:38:17 -0300

    Mudança While

--- src/Fahrenheit.java
+++ src/Fahrenheit.java
@@ -20,14 +20,13 @@ public class Fahrenheit {
         
         celsius = 10;
         
-        while (celsius <= 100);{
+        while (celsius <= 100){
         
         fahrenheit = (9 * celsius + 160) / 5;
         
         System.out.printf("%.2f°C = %.2f°F.\n", celsius, fahrenheit);
         
         celsius += 10;
-        
         }
         // TODO code application logic here
     }
